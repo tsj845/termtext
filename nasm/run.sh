aae849d454d7f9a33dbaee3650e6d55b1ef89499
Tristan <tschartman41@gmail.com>
2023-09-15 15:38:56 -0400

    Update run.sh

--- nasm/run.sh
+++ nasm/run.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
 # nasm -fmacho64 $1.asm && ld -macosx_version_min 10.7.0 -o $1 $1.o
 nasm -fmacho64 $1.asm && gcc $1.o -o $1 -Wl,-no_pie
+
+if [ $? != 0 ]
+then
+exit 1
+fi
 
 ____TTY_SETTINGS____=$(stty -g)
 
