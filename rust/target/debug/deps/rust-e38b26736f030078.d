4224c167705d4e9198c5a874de55200c7618e3db
Tristan <tschartman41@gmail.com>
2023-11-02 13:43:04 -0400

    e

--- rust/target/debug/deps/rust-e38b26736f030078.d
+++ rust/target/debug/deps/rust-e38b26736f030078.d
@@ -1,7 +1,6 @@
-/Users/tschartman/Documents/GitHub/termtext/rust/target/debug/deps/rust-e38b26736f030078.rmeta: src/lib.rs src/line.rs src/doc.rs
+/Users/tschartman/Documents/GitHub/termtext/rust/target/debug/deps/rust-e38b26736f030078.rmeta: src/lib.rs src/line.rs
 
-/Users/tschartman/Documents/GitHub/termtext/rust/target/debug/deps/rust-e38b26736f030078.d: src/lib.rs src/line.rs src/doc.rs
+/Users/tschartman/Documents/GitHub/termtext/rust/target/debug/deps/rust-e38b26736f030078.d: src/lib.rs src/line.rs
 
 src/lib.rs:
 src/line.rs:
-src/doc.rs:
--- rust/target/debug/deps/rust-ea78762bbcafc2f5.d
+++ rust/target/debug/deps/rust-ea78762bbcafc2f5.d
@@ -1,7 +1,6 @@
-/Users/tschartman/Documents/GitHub/termtext/rust/target/debug/deps/rust-ea78762bbcafc2f5.rmeta: src/lib.rs src/line.rs src/doc.rs
+/Users/tschartman/Documents/GitHub/termtext/rust/target/debug/deps/rust-ea78762bbcafc2f5.rmeta: src/lib.rs src/line.rs
 
-/Users/tschartman/Documents/GitHub/termtext/rust/target/debug/deps/rust-ea78762bbcafc2f5.d: src/lib.rs src/line.rs src/doc.rs
+/Users/tschartman/Documents/GitHub/termtext/rust/target/debug/deps/rust-ea78762bbcafc2f5.d: src/lib.rs src/line.rs
 
 src/lib.rs:
 src/line.rs:
-src/doc.rs:
